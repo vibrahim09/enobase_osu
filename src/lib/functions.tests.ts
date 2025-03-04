6bb16672407b27ea81e908dbe0f06d27d9ea75e8
akinsmosu <akinsm@oregonstate.edu>
2025-03-03 16:54:47 -0800

    Adds unit tests for number functions, refactors failing tests

--- src/lib/functions.tests.ts
+++ src/lib/functions.tests.ts
@@ -1,21 +1,46 @@
-import {dispatch } from  './functions'
+import {dispatch, DivideRequestBody, MedianRequestBody, MultiplyRequestBody} from './functions'
 import {AddRequestBody, SubtractRequestBody, ListRequestBody, AtRequestBody, FirstRequestBody, 
     LastRequestBody,SortRequestBody, ReverseRequestBody,ConcatRequestBody,SomeRequestBody, FunctionRequestBody} from './functions'
 describe('dispatch function', () => {
     it('should return the sum of two numbers', () => {
       const reqBody: AddRequestBody = { function: 'add', num1: 1, num2: 2 }
       const result = dispatch(reqBody)
-      expect(result).toBe(3)
+      expect(result).toEqual({ result: 3 })
     })
     it('should return the difference of two numbers', () => {
       const reqBody: SubtractRequestBody = { function: 'subtract', num1: 5, num2: 2 }
       const result = dispatch(reqBody)
-      expect(result).toBe(3)
+      expect(result).toEqual({ result: 3 })
+    })
+    it('should return the product of two numbers', () => {
+        const reqBody: MultiplyRequestBody = { function: 'multiply', num1: 5, num2: 2 }
+        const result = dispatch(reqBody)
+        expect(result).toEqual({ result: 10 })
+    })
+    it('should return the quotient of two numbers', () => {
+        const reqBody: DivideRequestBody = { function: 'divide', num1: 10, num2: 2 }
+        const result = dispatch(reqBody)
+        expect(result).toEqual({ result: 5 })
+    })
+    it('should return an error when dividing by 0', () => {
+        const reqBody: DivideRequestBody = { function: 'divide', num1: 10, num2: 0 }
+        const result = dispatch(reqBody)
+        expect(result).toEqual({ error: "Division by zero is not allowed" })
+    })
+    it('should return the middle number of an odd number of items', () => {
+        const reqBody: MedianRequestBody = { function: 'median', numbers: [1, 2, 3, 4, 5] }
+        const result = dispatch(reqBody)
+        expect(result).toEqual({ result: 3 })
+    })
+    it('should return the average of the middle two numbers of an even number of items', () => {
+        const reqBody: MedianRequestBody = { function: 'median', numbers: [2, 4, 6, 8] }
+        const result = dispatch(reqBody)
+        expect(result).toEqual({ result: 5 })
     })
     it('should return the list of items', () => {
       const reqBody: ListRequestBody<number> = { function: 'list', items: [1, 2, 3] }
       const result = dispatch(reqBody)
-      expect(result).toEqual([1, 2, 3])
+      expect(result).toEqual({ result: [1, 2, 3] })
     })
     it('should return the item at the given index', () => {
       const reqBody: AtRequestBody<string> = { function: 'at', items: ["hello", "kk", "mix"], index: 1 }
